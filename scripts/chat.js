dbda83f1ae56d2ff04949b037e3cf8d47fde3b62
Donovan <donovant78@gmail.com>
2022-04-06 11:28:09 -0400

    Render data to page

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,9 +1,20 @@
-function getMessages() {
+// Fetch the messages from PHP with AJAX request
+// And then render it into the HTML page with jQuery
+function getMessagesAndRender() {
 	$.get("getMessages.php", function (data) {
-		console.log(data)
+		renderMessages(data)
 	})
 }
 
+// Use the Json and transform it into HTML with jQuery
+function renderMessages(data) {
+	$("#chat-messages").html("")
+	data.forEach(function (message) {
+		$("#chat-messages").append(`<div class="message">${message.username} : ${message.text}</div>`)
+	})
+}
+
+// Refresh the messages every 2 seconds
 setInterval(() => {
-	getMessages()
+	getMessagesAndRender()
 }, 2000)
